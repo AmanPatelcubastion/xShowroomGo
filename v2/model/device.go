a2f630c5cc4fe446797f06fbb2ccc1c4fbd227b2
Aatish Rana <aatishrana8@gmail.com>
2017-12-05 17:31:00 +0530

    added database in v2

---
+++ v2/model/device.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	"github.com/aatishrana/GraphQLTesting/xShowroom/v2/database"
+)
+
+type Device struct {
+	Id     int        `gorm:"AUTO_INCREMENT" json:"id,omitempty"`
+	UUID   string        `gorm:"column:uuid" json:"uuid,omitempty"`
+	UserId int         `gorm:"column:user_id" json:"user_id,omitempty"`
+}
+
+func GetDevice(id int) Device {
+	data := Device{}
+	database.SQL.Debug().First(&data, id)
+	return data
+}
+
+func GetDevices() []Device {
+	data := []Device{}
+	database.SQL.Debug().Find(&data)
+	return data
+}
+
+func CreateDevice(uuid string, userId int) Device {
+
+	data := Device{UUID: uuid}
+
+	if userId != -1 {
+		data = Device{UUID: uuid, UserId: userId}
+	}
+
+	database.SQL.Create(&data)
+	return data
+}
+
+func UpdateDevice(id int, uuid string, userId int) Device {
+	oldData := Device{Id: id}
+	newData := Device{Id: id, UUID: uuid}
+
+	if userId != -1 {
+		newData = Device{Id: id, UUID: uuid, UserId: userId}
+	}
+
+	database.SQL.Model(&oldData).Updates(newData)
+	return newData
+}
+
+func GetDeviceOfUser(userId int) Device {
+	data := Device{}
+	database.SQL.Debug().First(&data, "user_id = (?)", userId)
+	return data
+}
---
+++ v2/mygraphql/device_resolver.go
@@ -0,0 +1,94 @@
+package mygraphql
+
+import (
+	"github.com/neelance/graphql-go"
+	"github.com/aatishrana/GraphQLTesting/xShowroom/v2/model"
+	"strconv"
+)
+
+//struct for graphql
+type device struct {
+	id          graphql.ID
+	device_uuid string
+	user        *user
+}
+
+//struct for upserting
+type deviceInput struct {
+	Id          *graphql.ID
+	Device_uuid string
+}
+
+//struct for response
+type deviceResolver struct {
+	device *device
+}
+
+func ResolveDevice(args struct{ ID graphql.ID }) []*deviceResolver {
+	var d []*deviceResolver
+	if args.ID != "" {
+		d = append(d, &deviceResolver{MapDevice(
+			model.GetDevice(convertId(args.ID)),
+		)})
+		return d
+	}
+	for _, val := range model.GetDevices() {
+		d = append(d, &deviceResolver{MapDevice(
+			val,
+		)})
+	}
+	return d
+}
+
+func ResolveCreateDevice(args *struct {
+	Device *deviceInput
+}) *deviceResolver {
+
+	var device = &device{}
+
+	if args.Device.Id == nil {
+		//create device
+		device = MapDevice(model.CreateDevice(args.Device.Device_uuid, -1)) //new device created set userId null
+	} else {
+		//update device
+		device = MapDevice(model.UpdateDevice(convertId(*args.Device.Id), args.Device.Device_uuid, -1)) //device updated keep userId whatever it was
+	}
+
+	return &deviceResolver{device}
+}
+
+//==================		fields resolvers		===========================
+
+func (r *deviceResolver) ID() graphql.ID {
+	return r.device.id
+}
+
+func (r *deviceResolver) DeviceUuid() string {
+	return r.device.device_uuid
+}
+
+//This method will run, if user is asked for
+func (r *deviceResolver) User() *userResolver {
+
+	if r.device != nil {
+		//if device not null get user of device from db and map
+		user := model.GetUserOfDevice(convertId(r.device.id))
+		return &userResolver{MapUser(user)}
+	}
+	return &userResolver{r.device.user}
+}
+
+//=================			mapper methods			==============================
+func MapDevice(modelDevice model.Device) *device {
+
+	if modelDevice == (model.Device{}) {
+		return &device{}
+	}
+
+	//create graphql (device) from model (device)
+	device := device{
+		id:          graphql.ID(strconv.Itoa(modelDevice.Id)),
+		device_uuid: modelDevice.UUID,
+	}
+	return &device
+}
